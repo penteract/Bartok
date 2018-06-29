fc49af0fa59e9114bfda69ebc6f9a26d126a4937
Angus Tayler <angler@clpc344.cs.ox.ac.uk>
2018-06-29 21:34:27 +0100

    Implement serialize

--- Serialize.hs
+++ Serialize.hs
@@ -1,12 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, StandaloneDeriving #-}
+
 module Serialize where
 
+import GHC.Generics
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
-
+import Data.Aeson (ToJSON,toEncoding,genericToEncoding,defaultOptions,toJSON,encode)
 
 import DataTypes
 import Views
 
-serialize :: GameView -> L.ByteString -- TODO(toby): change when Views is a thing
-serialize gs = ""
+deriving instance Generic GameView
+deriving instance Generic CardView
+deriving instance Generic Rank
+deriving instance Generic Suit
+
+instance ToJSON Rank where
+  toEncoding = genericToEncoding defaultOptions
+instance ToJSON Suit where
+  toEncoding = genericToEncoding defaultOptions
+instance ToJSON CardView where
+  toEncoding = genericToEncoding defaultOptions
+instance ToJSON GameView where
+  toEncoding = genericToEncoding defaultOptions
+
+serialize :: GameView -> L.ByteString
+serialize = encode . toJSON
+
+unserialize :: L.ByteString -> Event
+unserialize = undefined
