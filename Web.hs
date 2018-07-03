1bbed1bf7117e32af97edf48a10377f8b79e49c2
Toby Cathcart Burn <toby.cathcartburn@pmb.ox.ac.uk>
2018-07-03 14:27:18 +0100

    Implement base server functionality

--- Web.hs
+++ Web.hs
@@ -1,15 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Text
+import Data.Text(Text,intercalate)
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L
+import System.Environment
 
 import Network.Wai.Handler.Warp
 import Network.Wai
 import Network.HTTP.Types.Header
 import Network.HTTP.Types
+import Network.HTTP.Types.Status
 
 import qualified Control.Concurrent.Map as CMap
 import Control.Concurrent.MVar
+--import Control.Monad.Trans.State
+import Control.Monad.State
+import Control.Monad.Reader
+
+import Language.Haskell.Interpreter
 
 import DataTypes
 import Lib
@@ -17,7 +24,8 @@ import Views
 
 import Serialize
 
-type GMap = CMap.Map Text (GameState,Game,GameViewer)
+type GData = (GameState,Game,Viewer)
+type GMap = CMap.Map Text GData
 
 makeNewGame = return$ newGame ["Toby","Angus"]
 
@@ -31,7 +39,15 @@ app games req resp = do
 
 onGet :: GMap -> Application
 onGet games req resp = do
-    let gameName = intercalate "/" (pathInfo req)
+    (case pathInfo req of
+        [] -> homepage
+        [gname] -> playPage gname
+        [gname,"newRule"] -> newRulePage gname
+        _ -> const$const($err404)) games req resp
+
+playPage :: Text -> GMap -> Application
+playPage gameName games req resp = do
+    --let gameName = intercalate "/" (pathInfo req)
     putStrLn$ show$ gameName
     mx <- CMap.lookup gameName games
     case mx of
@@ -41,28 +57,100 @@ onGet games req resp = do
         Just a -> return ()
     resp$ responseFile ok200 [] "index.html" Nothing
 
-onPost :: GMap -> Application
-onPost games req resp = do
-    let gameName = intercalate "/" (pathInfo req)
+newRulePage :: Text -> GMap -> Application
+newRulePage gameName games req resp= do
     putStrLn$ show$ gameName
     mx <- CMap.lookup gameName games
     case mx of
+        Nothing -> do
+                gs <- makeNewGame
+                CMap.insertIfAbsent gameName (gs,baseAct,defaultView) games
+        Just a -> return ()
+    resp$ responseFile ok200 [] "newRule.html" Nothing
+
+homepage :: GMap -> Application
+homepage games req resp = resp err404
+
+doWithGame :: WithGame Response -> GMap -> Text -> Application
+doWithGame wg games gname req resp = do
+    mx <- CMap.lookup gname games
+    case mx of
         Nothing -> resp$ responseLBS badRequest400 [] ""
-        Just (gs,g,v) -> do
-            putStrLn "here"
-            ev <- lazyRequestBody req
-            print$ ev
-            case unserialize ev >>= (\e ->(,) e <$> getName e) of
-                Just (e,n) -> do
-                    let gs' = g e gs
-                    CMap.insert gameName (gs',g,v) games
-                    resp$ responseLBS ok200 [(hContentType,"application/json")] (serialize (v n gs'))
-                _ -> resp$ responseLBS badRequest400 [] ""
+        Just gd -> do
+            rb <- lazyRequestBody req
+            putStrLn "Requst Body:"
+            print$ rb
+            (r,gd') <- runStateT (runReaderT wg (req,rb)) gd
+            CMap.insert gname gd' games
+            resp$ r
 
 
+onPost :: GMap -> Application
+onPost games req resp = do
+    let gameName = intercalate "/" (pathInfo req)
+    case pathInfo req of
+        [gname] -> doWithGame playMove games gname req resp
+        [gname, "newRule"] -> doWithGame newRule games gname req resp
+        _ -> resp$ err404
 
+
+type WithGame a = ReaderT (Request,L.ByteString) (StateT GData IO) a
+
+getGS :: WithGame GameState
+getGS = gets (\(st,act,vw)->st)
+setGS :: GameState -> WithGame ()
+setGS st = modify (\(_,act,vw)->(st,act,vw))
+
+getViewer :: WithGame Viewer
+getViewer = gets (\(st,act,vw)->vw)
+
+getGame :: WithGame Game
+getGame = gets (\(st,act,vw)->act)
+
+getBody :: WithGame L.ByteString
+getBody = asks snd
+
+fromErr (WontCompile errs) = Prelude.unlines (map frghc errs)
+
+frghc (GhcError{errMsg=m}) = m
+
+playMove :: WithGame Response
+playMove = do
+    e <- getBody
+    case unserialize e >>= (\e -> (,) e <$> getName e) of
+        Just (e,n) -> do
+            g <- getGame
+            v <- getViewer
+            gs <- getGS
+            let gs' = g e gs
+            setGS gs'
+            return$ jsonResp (serialize (v n gs'))
+        Nothing -> return err404
+
+newRule :: WithGame Response
+newRule = do
+    b <- asks snd
+    f <- liftIO$ runInterpreter$ interpret (L.unpack b) (as::Rule)
+    case f of
+        Left err -> return$ responseLBS unprocessableEntity422 [] (L.pack$ show err)
+        Right r -> do
+            modify (\(st,act,vw)->(st,r act, vw))
+            return$ responseLBS ok200 [(hContentType,"application/json")] "{}"
+
+jsonResp = responseLBS ok200 [(hContentType,"application/json")]
 
 main :: IO ()
 main = do
+    args <- getArgs
+    let port = case args of
+                (n:_) -> (read n ::Int)
+                [] -> 8080
     games <- CMap.empty
-    run 8080 (app games)
+    run port (app games)
+
+err404 = responseLBS notFound404 []
+  "<html><head><title>404: Page Not Found</title></head>\
+    \ <body><h1>Page Not Found</h1></body></html>"
+err400 = responseLBS badRequest400 []
+  "<html><head><title>400: Bad Request</title></head>\
+    \ <body><h1>Bad Request</h1></body></html>"
